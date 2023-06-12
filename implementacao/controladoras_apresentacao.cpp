357e0b5938ad8cf73f1820da5726229f225ef733
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2023-06-12 17:01:50 -0300

    Camada de apresentação basicamente concluída. As controladoras foram implementadas, mas ainda há alguns problemas: os métodos executar de CtrlAprDev e de CtrlAprTest talvez não estejam funcionando direito. As linhas onde há comentários de 'if' é onde ficam os problemas.

--- implementacao/controladoras_apresentacao.cpp
+++ implementacao/controladoras_apresentacao.cpp
@@ -82,3 +82,278 @@ void CtrlAprInicial::executar() {
         }
     }
 }
+
+
+// *****Controladora Apresentação Autenticação:
+void CtrlAprAutentic::setCtrlServiceAutentic(InterServiceAutentic *ctrl) {
+    CtrlServiceAutentic = ctrl;
+}
+
+bool CtrlAprAutentic::autenticar(MATRICULA* Matricula) {
+    string matriculaStr, senhaStr;
+
+    while(true) {
+        system("cls");
+
+        cout << "+-+-Sistema de Gerenciamento de Testes-+-+" << endl;
+        cout << "Autenticacao de Desenvolvedor " << endl;
+        cout << "Digite sua Matricula: ";
+        cin >> matriculaStr;
+        cout << "Digite sua Senha: ";
+        cin >> senhaStr;
+
+        MATRICULA objMatricula;
+        try{
+            objMatricula.setValor(matriculaStr);
+        }
+        catch (const invalid_argument& ex) {
+            cout << "Erro => " << ex.what() << endl;
+        }
+
+        SENHA objSenha;
+        try{
+            objSenha.setValor(senhaStr);
+        }
+        catch (const invalid_argument& ex) {
+            cout << "Erro => " << ex.what() << endl;
+        }
+
+        bool autenticado = CtrlServiceAutentic->autenticar(objMatricula, objSenha);
+        if (autenticado) {
+            Matricula->setValor(matriculaStr);
+            return true;
+        }
+        else {
+            cout << "Matricula ou Senha incorretos.\nPressione qualquer tecla para continuar ou ESC para sair." << endl;
+            if (getch() == 27) return false; // 27 == ESC;
+        }
+    }
+}
+
+
+// *****Controladora Apresentação Desenvolvedor:
+void CtrlAprDev::setCtrlServiceDev(InterServiceDev *ctrl) {
+    CtrlServiceDev = ctrl;
+}
+
+void CtrlAprDev::executar(MATRICULA* Matricula) {
+    string senhaStr;
+    SENHA objSenha;
+    char escolha;
+
+    while(true) {
+        system("cls");
+
+        cout << "+-+-Sistema de Gerenciamento de Testes-+-+" << endl;
+        cout << "Desenvolvedor: " << Matricula->getValor() << endl;
+        cout << "1 - Alterar Senha" << endl;
+        cout << "2 - Descadastrar Usuario" << endl;
+        cout << "3 - Sair" << endl;
+
+        switch(getch()) {
+            case '1':
+                system("cls");
+                cout << "+-+-Sistema de Gerenciamento de Testes-+-+" << endl;
+                cout << "Desenvolvedor: " << Matricula->getValor() << endl;
+                cout << "Digite sua nova senha: ";
+                cin >> senhaStr;
+
+                objSenha.setValor(senhaStr);
+
+                //if (CtrlServiceDev->editar(Desenvolvedor(*Matricula, objSenha))) {
+                if (CtrlServiceDev->editar(Desenvolvedor())) {
+                    cout << "Senha alterada com sucesso." << endl;
+                }
+                else {
+                    cout << "Erro ao alterar a senha." << endl;
+                }
+                getch();
+                break;
+            case '2':
+                cout << "Deseja decadastrar o usuario desta conta? (s/n)" << endl;
+                escolha = getch();
+
+                if (escolha == 's' || escolha == 'S') {
+                    if (CtrlServiceDev->descadastrar(*Matricula)) {
+                        cout << "Usuario descadastrado com sucesso." << endl;
+                        Matricula->setValor("");
+                        getch();
+                        return;
+                    }
+                    else {
+                        cout << "Erro ao descadastrar usario." << endl;
+                        getch();
+                    }
+                }
+                break;
+            case '3':
+                return;
+            default:
+                cout << "Opcao invalida." << endl;
+                getch();
+                break;
+        }
+    }
+}
+
+void CtrlAprDev::cadastrar() {
+    string matriculaStr, senhaStr;
+    MATRICULA objMatricula;
+    SENHA objSenha;
+
+    while(true) {
+        system("cls");
+        cout << "+-+-Sistema de Gerenciamento de Testes-+-+" << endl;
+        cout << "Cadastro de Usuario" << endl;
+        cout << "Digite a Matricula: ";
+        cin >> matriculaStr;
+        cout << "Digite a Senha: ";
+        cin >> senhaStr;
+
+        objMatricula.setValor(matriculaStr);
+        objSenha.setValor(senhaStr);
+
+        //if (CtrlServiceDev->cadastrar(Desenvolvedor(objMatricula, objSenha))) {
+        if (CtrlServiceDev->cadastrar(Desenvolvedor())) {
+            cout << "Usuario cadastrado com sucesso." << endl;
+            getch();
+            return;
+        }
+        else {
+            cout << "Erro ao cadastrar usuario." << endl;
+            cout << "Pressione qualquer tecla ou ESC para sair." << endl;
+            if (getch() == 27) {
+                return;
+            }
+        }
+    }
+}
+
+
+// *****Controladora Apresentação Testes/Casos de Teste:
+void CtrlAprTest::setCtrlServiceTest(InterServiceTest *ctrl) {
+    CtrlServiceTest = ctrl;
+}
+
+void CtrlAprTest::executar(CODIGO* Codigo) {
+    char escolha;
+
+    string classeStr;
+    CLASSE objClasse;
+    string textoStr;
+    TEXTO objTexto;
+    string nomeStr;
+    TEXTO objNome;
+
+    Testes objTeste;
+    CasoDeTeste objCasoDeTeste;
+
+    while (true) {
+        system("cls");
+        cout << "+-+-Sistema de Gerenciamento de Testes-+-+" << endl;
+        cout << "Teste: " << Codigo->getValor() << endl;
+        cout << "1 - Editar Classe do Teste" << endl;
+        cout << "2 - Editar Texto associado ao Teste" << endl;
+        cout << "3 - Descadastrar Teste" << endl;
+        cout << "4 - Editar Nome do Caso de Teste" << endl;
+        cout << "5 - Descadastrar Caso de Teste" << endl;
+        cout << "6 - Sair" << endl;
+
+        switch(getch()) {
+            case '1':
+                system("cls");
+                cout << "+-+-Sistema de Gerenciamento de Testes-+-+" << endl;
+                cout << "Digite a nova Classe do Teste: ";
+                cin >> classeStr;
+
+                objTeste.setClasseTeste(classeStr);
+
+                //if (CtrlServiceTest->editarTeste(Testes(*Codigo, objClasse))) {
+                if (CtrlServiceTest->editarTeste(Testes())) {
+                    cout << "Classe do teste alterada com sucesso." << endl;
+                }
+                else {
+                    cout << "Erro ao alterar a classe." << endl;
+                }
+                getch();
+                break;
+
+            case '2':
+                system("cls");
+                cout << "+-+-Sistema de Gerenciamento de Testes-+-+" << endl;
+                cout << "Digite o novo Texto associado ao Teste: ";
+                cin >> textoStr;
+
+                objTeste.setTextoTeste(textoStr);
+
+                //if (CtrlServiceTest->editarTeste(Testes(*Codigo, objClasse))) {
+                if (CtrlServiceTest->editarTeste(Testes())) {
+                    cout << "Texto associado ao Teste alterado com sucesso." << endl;
+                }
+                else {
+                    cout << "Erro ao alterar o Texto associado ao Teste." << endl;
+                }
+                getch();
+                break;
+
+            case '3':
+                cout << "Deseja decadastrar o teste? (s/n)" << endl;
+                escolha = getch();
+
+                if (escolha == 's' || escolha == 'S') {
+                    if (CtrlServiceTest->descadastrarTeste(*Codigo)) {
+                        cout << "Teste descadastrado com sucesso." << endl;
+                        getch();
+                        return;
+                    }
+                    else {
+                        cout << "Erro ao descadastrar teste." << endl;
+                        getch();
+                    }
+                }
+                break;
+
+            case '4':
+                system("cls");
+                cout << "+-+-Sistema de Gerenciamento de Testes-+-+" << endl;
+                cout << "Digite o novo nome do Caso de Teste: ";
+                cin >> nomeStr;
+
+                objCasoDeTeste.setCasoNome(nomeStr);
+
+                //if (CtrlServiceTest->editarCasoDeTeste(CasoDeTeste(*Codigo, objNome))) {
+                if (CtrlServiceTest->editarCasoDeTeste(CasoDeTeste())) {
+                    cout << "Nome do Caso de Teste alterado com sucesso." << endl;
+                }
+                else {
+                    cout << "Erro ao alterar o nome." << endl;
+                }
+                getch();
+                break;
+
+            case '5':
+                cout << "Deseja decadastrar o Caso de Teste? (s/n)" << endl;
+                escolha = getch();
+
+                if (escolha == 's' || escolha == 'S') {
+                    if (CtrlServiceTest->descadastrarCasoDeTeste(*Codigo)) {
+                        cout << "Caso de Teste descadastrado com sucesso." << endl;
+                        getch();
+                        return;
+                    }
+                    else {
+                        cout << "Erro ao descadastrar Caso de Teste." << endl;
+                        getch();
+                    }
+                }
+                break;
+
+            case '6':
+                return;
+            default:
+                cout << "Opcao invalida." << endl;
+                getch();
+                break;
+        }
+    }
+}
